d5b0caaecd6b3166deeb8c1ac4d3c42fec0ef0ba
NTierSoftware <ntiersoftwareengineering@gmail.com>
2016-02-15 21:16:08 -0800

    SecurityGuard class gen CSR

--- app/src/main/java/org/peacekeeper/crypto/SecurityGuard.java
+++ app/src/main/java/org/peacekeeper/crypto/SecurityGuard.java
@@ -1,4 +1,6 @@
 package org.peacekeeper.crypto;
+//TODO use PEMWriter class to print
+
 /*PeaceKeeper Cryptographic Security Policy:
 
         Asymmetric Key Generation : ECDSA with 256 bits
@@ -12,11 +14,13 @@ package org.peacekeeper.crypto;
         Password Encryption : bcrypt
 */
 
-import org.peacekeeper.exception.pkErrCode;
-import org.peacekeeper.exception.pkException;
+import org.peacekeeper.exception.*;
+//import org.peacekeeper.exception.pkException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.spongycastle.asn1.pkcs.Attribute;
+
+import org.spongycastle.asn1.x500.*;
+import org.spongycastle.asn1.x500.style.BCStrictStyle;
 import org.spongycastle.jce.ECNamedCurveTable;
 import org.spongycastle.jce.spec.ECParameterSpec;
 import org.spongycastle.operator.ContentSigner;
@@ -26,36 +30,26 @@ import org.spongycastle.pkcs.PKCS10CertificationRequest;
 import org.spongycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.spongycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 
-//import org.spongycastle.pkcs.bc.BcPKCS10CertificationRequestBuilder;
-
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
-import java.security.Signature;
-import java.security.SignatureException;
-
-import javax.security.auth.x500.X500Principal;
+import java.security.*;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.util.ContextInitializer;
 
-
+//import org.bouncycastle.openssl.PEMWriter;
+import org.spongycastle.openssl.jcajce.JcaPEMWriter;
 // http://stackoverflow.com/questions/18244630/elliptic-curve-with-digital-signature-algorithm-ecdsa-implementation-on-bouncy
 public class SecurityGuard {
 //begin static
 static private final LoggerContext		mLoggerContext	= (LoggerContext)LoggerFactory.getILoggerFactory();
-static private final ContextInitializer	mContextInitializer		= new ContextInitializer( mLoggerContext );
-static private final Logger				mLog	= LoggerFactory.getLogger( SecurityGuard.class );
+static private final ContextInitializer	mContextInitializer	= new ContextInitializer( mLoggerContext );
+static private final Logger				mLog = LoggerFactory.getLogger( SecurityGuard.class );
 static private KeyPair keyPair = null;
-static private final String spongeyCastle = "SC",
-                            SHA256withECDSA = "SHA256withECDSA";
+static private final String SpongeyCastle = "SC",
+                            SHA256withECDSA = "SHA256withECDSA",
+                            charsetname = "UTF-8";
 //end static
 
 private String message = null;
@@ -67,12 +61,12 @@ public KeyPair GenerateKeys()
 {   if (keyPair == null) {
         try {
             ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("P-256");
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("ECDSA", spongeyCastle);
-            kpg.initialize(ecSpec, new SecureRandom());
-            keyPair = kpg.generateKeyPair(); }
-        catch (NoSuchAlgorithmException| NoSuchProviderException| InvalidAlgorithmParameterException x)
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", SpongeyCastle );
+            keyPairGenerator.initialize(ecSpec, new SecureRandom());
+            keyPair = keyPairGenerator.generateKeyPair(); }
+        catch (NoSuchAlgorithmException| NoSuchProviderException| InvalidAlgorithmParameterException X)
         {   keyPair = null;
-            pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("GenerateKeys err", x);;
+            pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("GenerateKeys err", X);;
             mLog.error(CRYPTOERR.toString());
             throw CRYPTOERR; }
     }//if
@@ -85,34 +79,34 @@ public byte[] getSignature(){
     if (signature == null)
     try {
         KeyPair pair = this.GenerateKeys();
-        Signature ecdsaSign = Signature.getInstance(SHA256withECDSA, spongeyCastle);
+        Signature ecdsaSign = Signature.getInstance(SHA256withECDSA, SpongeyCastle);
         ecdsaSign.initSign(pair.getPrivate());
-        ecdsaSign.update(this.message.getBytes("UTF-8"));
+        ecdsaSign.update(this.message.getBytes(charsetname));
         signature = ecdsaSign.sign();
     } catch (NoSuchAlgorithmException| NoSuchProviderException|
-             InvalidKeyException| UnsupportedEncodingException| SignatureException x)
+             InvalidKeyException| UnsupportedEncodingException| SignatureException X)
     {
-        pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("Crypto Signature err", x);
+        pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("Crypto Signature err", X);
         mLog.error(CRYPTOERR.toString());
         signature = null;
         throw CRYPTOERR;
     }//catch
 
-    return signature;
+return signature;
 }//getSignature
 
 public boolean verify(){
     Signature ecdsaVerify = null;
     boolean retVal = false;
     try {
-        ecdsaVerify = Signature.getInstance(SHA256withECDSA, spongeyCastle);
+        ecdsaVerify = Signature.getInstance(SHA256withECDSA, SpongeyCastle);
         ecdsaVerify.initVerify(GenerateKeys().getPublic());
-        ecdsaVerify.update(this.message.getBytes("UTF-8"));
+        ecdsaVerify.update(this.message.getBytes(charsetname));
         retVal = ecdsaVerify.verify( getSignature() );
     } catch (NoSuchAlgorithmException| NoSuchProviderException|
-             SignatureException| UnsupportedEncodingException x)
+             SignatureException| UnsupportedEncodingException X)
     {   retVal = false;
-        pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("crypto verify err", x);
+        pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("crypto verify err", X);
         mLog.error(CRYPTOERR.toString()); }
 
     finally { return retVal; }
@@ -121,7 +115,7 @@ public boolean verify(){
 // http://stackoverflow.com/questions/9661008/compute-sha256-hash-in-android-java-and-c-sharp?lq=1
 public void setHash() throws NoSuchAlgorithmException, UnsupportedEncodingException
 {   MessageDigest digest = MessageDigest.getInstance("SHA-256");
-    this.hash = digest.digest(message.getBytes("UTF-8"));
+    this.hash = digest.digest(message.getBytes(charsetname));
 }//setHash
 
 
@@ -139,34 +133,46 @@ public String toString() {
     StringBuilder retVal = new StringBuilder("SecurityGuard:\t").append(this.message)
                             .append("\tHash: ").append(hashStr);
 
-    return retVal.toString();
+return retVal.toString();
 }
 
 // http://stackoverflow.com/questions/20532912/generating-the-csr-using-bouncycastle-api
 public PKCS10CertificationRequest generateCSR(){
     KeyPair pair = this.GenerateKeys();
 
+    //TODO fix X500 EmailAddress
+    X500Name subject = new X500NameBuilder( new BCStrictStyle() )
+            .addRDN(BCStrictStyle.EmailAddress, "JD.John.Donaldson@gmail.com")
+            .build();
+
     PKCS10CertificationRequestBuilder p10Builder = new JcaPKCS10CertificationRequestBuilder(
-                                                       new X500Principal("CN=\"JD\"  Requested Test Certificate") //CN = common name
-                                                       , pair.getPublic())
-                                                   .setLeaveOffEmptyAttributes(true);
+            subject
+            , pair.getPublic() )
+            .setLeaveOffEmptyAttributes(true);
 
     JcaContentSignerBuilder csBuilder = new JcaContentSignerBuilder(SHA256withECDSA);
     ContentSigner signer = null;
+
     try { signer = csBuilder.build(pair.getPrivate()); }
-    catch (OperatorCreationException x) {
-        pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("CSR err", x);
+    catch (OperatorCreationException X) {
+        pkException CRYPTOERR = new pkException(pkErrCode.CRYPTO).set("CSR err", X);
         mLog.error(CRYPTOERR.toString());
         throw CRYPTOERR;
     }
 
-    //PKCS10CertificationRequest PKCS10csr = p10Builder.build(signer);
-
-    //Attribute[] attr =  PKCS10csr.getAttributes();
-
-    return p10Builder.build(signer);
-
+return p10Builder.build(signer);
 }//generateCSR
+
+
+public void PEMprint() {
+//import org.bouncycastle.openssl.PEMWriter;
+
+
+    OutputStreamWriter output = new OutputStreamWriter(System.out);
+    JcaPEMWriter pem = new JcaPEMWriter(output);
+    pem.writeObject(this);
+    pem.close();
+}//PEMprint
 
 }//SecurityGuard
 
